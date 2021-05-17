f48df520412d83a9ab71488958efd7044861ca5a
pelek_s <pawel.sioda@gmail.com>
2021-05-17 19:55:26 +0200

    Add eslint and eslint config file.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-use-before-define': ['error', { functions: false, classes: false }],
+  },
+};
