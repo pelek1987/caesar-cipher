b52a496d84c3c92f24d2bbc2919dbf9b82f2c972
pelek_s <pawel.sioda@gmail.com>
2021-05-17 20:27:49 +0200

    Add prettier as eslint rule and turn off eslint formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,22 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
+  plugins: ["prettier"],
   rules: {
-    'no-unused-vars': 'warn',
-    'no-use-before-define': ['error', { functions: false, classes: false }],
+    "no-unused-vars": "warn",
+    "no-use-before-define": ["error", { functions: false, classes: false }],
+    "prettier/prettier": [
+      "warn",
+      {
+        semi: true,
+        trailingComma: "all",
+        printWidth: 150,
+      },
+    ],
   },
 };
