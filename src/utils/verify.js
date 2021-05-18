2366b4f133792e237d0bb98e836a79a3c2175b9f
pelek_s <pawel.sioda@gmail.com>
2021-05-18 06:43:00 +0200

    Create verify function in utils

---
+++ src/utils/verify.js
@@ -0,0 +1,8 @@
+const verify = (input, goal) => {
+  if (input === goal) {
+    console.log('Gratulacje!');
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
+};
+export default verify;
